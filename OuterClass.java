398705877b2d6a36bb1172f40cb1c2c757c2359b
rchuhi <94059813+rchuhi@users.noreply.github.com>
2022-03-13 13:49:12 -0400

    Add files via upload

---
+++ OuterClass.java
@@ -0,0 +1,28 @@
+/*Reginah Chuhi
+	CIS221
+	Tuesday and Thursday 2:10pm*/
+
+public class OuterClass {
+
+	private String name="Shadows";
+	
+			
+		
+//Create a getter and setter to get the name the private class
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			
+			this.name = name;
+		}
+		//Nested InnerClass
+		static class InnerClass {
+		    int year = 2019;
+		  }
+		}
+
+	
+
+
